b6c6fa60b47a7120f6a9b6e7cfdb31383decc83f
Sumanth Balusu <sumanth.balusu.5@gmail.com>
2020-08-12 02:04:35 +0200

    On Click Button

--- SAO.js
+++ SAO.js
@@ -1,3 +1,7 @@
-document.getElementById("Heading").addEventListener("click",function(){
-    document.getElementById("Heading").innerHTML = "This is SAO"
+/*document.getElementById("Heading").addEventListener("click",function SAO1(){
+    document.getElementById("Rock").innerHTML = "This is SAO"
 })
+*/
+function SAO1() {
+    document.getElementById("Heading").innerHTML="Boku no Namaiva SAO des!"
+}
